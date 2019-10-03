569a5a938ac9a6223813d6e02c7a48b00661e15b
carlromeo <carlromeo.dogbey@gmail.com>
2019-10-03 17:05:37 -0400

    Initial commit/setup

---
+++ src/main/java/com/example/demo/Guestbook.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Guestbook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=3, max = 20)
+    private String name;
+
+    @NotNull
+    @Min(1)
+    private int date;
+
+    @NotNull
+    @Size(min=3, max = 250)
+    private String wishes;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getWishes() {
+        return wishes;
+    }
+
+    public void setWishes(String wishes) {
+        this.wishes = wishes;
+    }
+}
